11038f853802677b36f549e9e3ad24c0937863e8
Seth <sethpatrickrobinson@gmail.com>
2026-06-08 11:10:41 -0500

    Creating expressions and parser

---
+++ parser.nim
@@ -0,0 +1,93 @@
+import ./token
+import ./exprsn
+
+type 
+    Parser* = object
+        tokens: seq[Token]
+        current: int
+
+proc expression(p: var Parser): Expr
+
+proc peek(p: var Parser): Token = 
+    result = p.tokens[p.current]
+
+proc previous(p: var Parser): Token = 
+    result = p.tokens[p.current - 1]
+
+proc isAtEnd(p: var Parser): bool = 
+    result = (p.peek().tkType) == tkEof
+
+proc advance(p: var Parser): Token = 
+    if (not p.isAtEnd()): inc p.current
+    result = p.previous()
+
+proc check(p: var Parser, t: TokenType): bool = 
+    if (not p.isAtEnd()): inc p.current
+    result = p.peek().tkType == t
+
+proc consume(p: var Parser, t: TokenType, message: string) = 
+    # todo
+    discard
+
+proc match(p: var Parser, types: varargs[TokenType]): bool = 
+    for t in types:
+        if (p.check(t)):
+            discard p.advance()
+            return true
+    
+    result = false
+
+proc primary(p: var Parser): Expr = 
+    if (p.match(tkFalse)): return newLiteral(initLiteral(false))
+    if (p.match(tkTrue)): return newLiteral(initLiteral(true))
+    if (p.match(tkNil)): return newLiteral(initLiteral())
+
+    if (p.match(tkNumber, tkString)): return newLiteral(p.previous().literal)
+
+    if (p.match(tkLeftParen)):
+        let ex = p.expression()
+        p.consume(tkRightParen, "Except ')' after expression.")
+        return newGrouping(ex)
+
+proc unary(p: var Parser): Expr = 
+    if (p.match(tkBang, tkMinus)):
+        let operator = p.previous()
+        let right = p.unary()
+        return newUnary(operator, right)
+
+    result = p.primary()
+
+proc factor(p: var Parser): Expr = 
+    var ex = p.unary()
+
+    while (p.match(tkSlash, tkStar)):
+        let operator = p.previous()
+        let right = p.unary()
+        ex = newBinary(ex, operator, right)
+    
+    result = ex
+
+proc comparison(p: var Parser): Expr =
+    var ex = p.factor()
+
+    while (p.match(tkMinus, tkPlus)):
+        let operator = p.previous()
+        let right = p.factor()
+        ex = newBinary(ex, operator, right)
+
+    result = ex
+
+proc equality(p: var Parser): Expr =
+    var ex = p.comparison()
+
+    while (p.match(tkBangEqual, tkEqualEqual)):
+        let operator = p.previous()
+        let right = p.comparison()
+        ex = newBinary(ex, operator, right)
+    
+    result = ex
+
+proc expression(p: var Parser): Expr = 
+    result = p.equality()
+
+
